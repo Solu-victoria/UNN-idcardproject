775df7171251cb3cd5c71af6f302db53b9f2fac4
eziashi favour <61447251+brainbox-feivel@users.noreply.github.com>
2022-10-16 19:16:37 +0100

    Add files via upload

---
+++ about.php
@@ -0,0 +1,35 @@
+<?php
+include "headercon.php";
+?>
+       
+    </div>
+	<ol class="breadcrumb">
+        <li class="breadcrumb-item">
+            <a href="index.php">Home</a>
+        </li>
+        <li class="breadcrumb-item active">About</li>
+    </ol>
+	 <!-- //banner-text -->
+	 <section class="banner_bottom1 py-md-5">
+		<div class="container py-4 mt-2">
+			<h3 class="heading-agileinfo text-center">About  <span>Us</span></h3>
+			<div class="inner_sec_info_wthree_agile mt-md-5 pt-3">
+				<div class="row help_full">
+					<div class="col-lg-6 banner_bottom_grid help">
+						<img src="images/g1.jpg" alt=" " class="img-fluid">
+					</div>
+					<div class="col-lg-6 banner_bottom_left1">
+						<h4>Welcome to UNN ID card </h4>
+						<p>Not having the proper photo ID can prevent you from doing many things. Think about your own security. You wouldn’t want someone using your personal credit score to open accounts or use your bank card to make purchases. Your photo identification protects you.</p>
+						<P>	An identity document (also called a piece of identification or ID, or colloquially as papers) is any document that may be used to prove a person's identity. If issued in a small, standard credit card size form, it is usually called an identity card (IC, ID card, citizen card), or passport card.</p>
+						</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	
+
+<?php
+include "footer.php";
+?>
---
+++ contact.php
@@ -0,0 +1,97 @@
+<?php
+include 'headercon.php';
+?>
+        <!-- //header -->
+       
+    </div>
+	 <!-- //banner-text -->
+	   <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+            <a href="index.php">Home</a>
+        </li>
+        <li class="breadcrumb-item active">Contact</li>
+    </ol>
+    <!-- contact -->
+    <section class="banner-bottom-w3ls pt-lg-5 pt-md-3 pt-3">
+        <div class="inner-sec-wthreelayouts pt-md-5 pt-md-3 pt-3">
+			<h2 class="heading-agileinfo text-center  mb-4">Get In <span> Touch</span></h2>
+			<div class="container pt-sm-5">
+                <div class="address row mb-5">
+                    <div class="col-lg-4 address-grid-w3l">
+                        <div class="row address-info">
+                            <div class="col-3 address-left text-center">
+                                <i class="far fa-map"></i>
+                            </div>
+                            <div class="col-9 address-right text-left">
+                                <h6 class="ad-info text-uppercase mb-2">Address</h6>
+                                <p> Enugu State, Nigeria.
+
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 address-grid-w3l">
+                        <div class="row address-info">
+                            <div class="col-3 address-left text-center">
+                                <i class="far fa-envelope"></i>
+                            </div>
+                            <div class="col-9 address-right text-left">
+                                <h6 class="ad-info text-uppercase mb-2">Email</h6>
+                                <p>Email :
+                                    <a href="#"> info@unn.edu.ng</a>
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-lg-4 address-grid-w3l">
+                        <div class="row address-info">
+                            <div class="col-3 address-left text-center">
+                                <i class="fas fa-mobile-alt"></i>
+                            </div>
+                            <div class="col-9 address-right text-left">
+                                <h6 class="ad-info text-uppercase mb-2">Phone</h6>
+                                <p>+234 816 697 6585</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6 map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d7922.472494789673!2d7.400869472911807!3d6.862265832170263!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sunn!5e0!3m2!1sen!2sng!4v1645130314450!5m2!1sen!2sng" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+                    </div>
+                    <div class="col-md-6 main_grid_contact">
+                        <div class="form">
+                            <h4 class="mb-4 text-left">Send us a message</h4>
+                            <form action="#" method="post">
+                                <div class="form-group">
+                                    <label class="my-2">Name</label>
+                                    <input class="form-control" type="text" name="Name" placeholder="" required="">
+                                </div>
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input class="form-control" type="email" name="Email" placeholder="" required="">
+                                </div>
+                                <div class="form-group">
+                                    <label>Message</label>
+                                    <textarea id="textarea" placeholder=""></textarea>
+                                </div>
+                                <div class="input-group1">
+                                    <input class="form-control" type="submit" value="Submit">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- //contact -->
+<!--/newsletter-->
+   <?php
+   include 'footer.php';
+   ?>
